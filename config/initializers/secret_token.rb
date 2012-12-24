99ed06b9543f8cd02b3c339da63c8f49b17c744c
fujita-y <y.fujita.lwp@gmail.com>
2012-12-24 11:25:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '99563fee0ba3bd0b0c3f5ddd5ad8a5eff90b5f0356ea1d6c1742023af1dd369d6db170b9f39cd0c7c3a683fcd1a3814792ab36a30c0379af2b963431389f106a'
